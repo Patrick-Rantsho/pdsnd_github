e2e915debaab81fcee4f70fc37d2ef072d243e56
Patrick Rantsho <rantsho.patrick@gmail.com>
2022-05-01 21:20:52 +0200

    Added a print statement in the end of the code

--- Bikeshare.py
+++ Bikeshare.py
@@ -255,7 +255,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-        print("The End")
+        print("Good! Provide Answers Again")
 
 
 
